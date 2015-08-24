30e9d9be588b470477863e5f10cb66288762a3f3
Markus Kobler <markus@distinctive.co>
2015-08-24 01:11:04 +0100

    working on broccoli plugin

--- test/fixtures/text/actual.js
+++ test/fixtures/text/actual.js
@@ -3,4 +3,6 @@
   <div>Hiya!</div>
   <div>{"First · Second"}</div>
   <div>{["First ", <span>&middot;</span>, " Second"]}</div>
+  <div>{foo}</div>
+  <div>{a.b.c}</div>
 </div>
--- test/fixtures/text/expected.js
+++ test/fixtures/text/expected.js
@@ -3,4 +3,6 @@ elementOpen("div");
   elementOpen("div");text("Hiya!");elementClose("div");
   elementOpen("div");text("First · Second");elementClose("div");
   elementOpen("div");text("First ");elementOpen("span");text("·");elementClose("span");text(" Second");elementClose("div");
+  elementOpen("div");text(foo);elementClose("div");
+  elementOpen("div");text(a.b.c);elementClose("div");
 elementClose("div");;
